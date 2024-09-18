a4eb4761feb93c0de899c03c965b5b9e45a61b79
Natalie Chen <z5592383@ad.unsw.edu.au>
2024-09-18 20:38:34 +1000

    Complete ex 1 DOM

--- exercises/js_dom_manipulation/ex1/ex1.js
+++ exercises/js_dom_manipulation/ex1/ex1.js
@@ -1,4 +1,19 @@
-console.log("Yay! The javascript is running")
+console.log("Yay! The javascript is running");
+
+const nameElement = document.getElementById('name');
+nameElement.textContent = "Natalie";
+
+const makeBigger = document.getElementById('bigger');
+makeBigger.style.fontSize = "500px" 
+makeBigger.style.color = "green";
+
+const imageElement = document.querySelector('img');
+imageElement.src = "https://cdn-prd.content.metamorphosis.com/wp-content/uploads/sites/6/2022/12/shutterstock_781327003-1-768x512.jpg";
+
+const hiddenSquares = document.getElementsByClassName('hidden');
+for (let i = 0; i < hiddenSquares.length; i++) {
+    hiddenSquares[i].style.display = 'block'; 
+}
 
 // Cat picture source 
 // https://cdn-prd.content.metamorphosis.com/wp-content/uploads/sites/6/2022/12/shutterstock_781327003-1-768x512.jpg
